fe1b59b3f5a433a6f5f6c951f087262486bd5c50
abhirock74 <abhishek.suvaidyam.com>
2022-08-13 16:46:25 +0530

    add login api

--- Backend/Api/Auth/Login.js
+++ Backend/Api/Auth/Login.js
@@ -1,16 +1,26 @@
 const User = require("../../Model/User");
+const JWT = require('jsonwebtoken');
+const JWT_SECRET = 'fhjkdfghdfgjkdfjkhgjkdfgj';
 
-module.exports= async(req, res)=>{
+const Login= async(req, res)=>{
     try {
-        let {email , password} = req.body;
-        if(!email || !password){
-            
-        }
-        let user = await User.findOne();
-        if(user){
-
-        }
+        let { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Bad Request: email and password are required' });
+        };
+        let user = await User.findOne({ email, password });
+        if (user) {
+            let token = JWT.sign({ _id: user._id, mobileNo: user.mobileNo }, JWT_SECRET);
+            return res.json({
+                message: 'Login Successful',
+                token: token
+            });
+        } else {
+            return res.status(400).json({ message: 'Bad Request: mobileNo and password are incorrect' });
+        };
     } catch (error) {
         return res.status(500).json({message: error.message})
     }
 }   
+
+module.exports = Login;
