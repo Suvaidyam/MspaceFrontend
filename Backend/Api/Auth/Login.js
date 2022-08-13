35f2af5962aaa4a79a7c8d636e15cdf093c41ef0
abhirock74 <abhishek.suvaidyam.com>
2022-08-13 16:07:58 +0530

    add space

--- Backend/Api/Auth/Login.js
+++ Backend/Api/Auth/Login.js
@@ -2,8 +2,15 @@ const User = require("../../Model/User");
 
 module.exports= async(req, res)=>{
     try {
-        let 
+        let {email , password} = req.body;
+        if(!email || !password){
+            
+        }
+        let user = await User.findOne();
+        if(user){
+
+        }
     } catch (error) {
         return res.status(500).json({message: error.message})
     }
-}
+}   
