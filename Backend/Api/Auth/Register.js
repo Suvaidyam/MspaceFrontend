57c0c56c9d594d4b6a5227329f0e8144cf31ef98
abhirock74 <abhishek.suvaidyam.com>
2022-08-16 19:43:22 +0530

    modify register api and server.js file

--- Backend/Api/Auth/Register.js
+++ Backend/Api/Auth/Register.js
@@ -1,26 +1,24 @@
 const User = require('../../Model/User');
 
 
-const register = async (req, res) => {
+const Register= async(req, res)=>{
     try {
-        let { name, password, email, company, userType } = req.body;
-        if (!name || !password || !email || !userType || !company) {
-            return res.status(400).json({ message: 'Bad request company,password,email,userType is require' });
-        }
-        let user = await User.findOne({ name, password, email, company, userType });
+        let { name, password, email, company, userType} = req.body;
+        if (!name || !password || !email || !company || !userType) {
+            return res.status(400).json({ message: "name  company,password,email,userType is require" })
+        };
+        let user = await User.findOne({ email, password });
         if (user) {
-            return res.status(400).json({ message: 'Dublicate ' });
-
+            return res.status(400).json({ message: "Dublicate User" });
         } else {
             user = await User.create(req.body);
-            return res.status(200).json({ massage: 'user successfully created', user: user })
+            return res.status(200).json({ message: "User Successfully Created", user: user });
         }
     } catch (error) {
-        return res.status(500).json({ massage: error.massage });
+        return res.status(500).json({ message: error.message });
     }
-
 }
 
 
 
-module.exports = register;
+module.exports = Register;
