11d2f28aaf366896df380e8e4df645c4366fb40f
abhirock74 <abhishek.suvaidyam.com>
2022-08-19 20:26:46 +0530

    add change in those API which is not workig Properly

--- Backend/Api/Auth/VerifyToken.js
+++ Backend/Api/Auth/VerifyToken.js
@@ -3,13 +3,17 @@ const JWT = require('jsonwebtoken');
 const JWT_SECRET = 'fhjkdfghdfgjkdfjkhgjkdfgj';
 
 module.exports = async (req, res, next) => {
+    // console.log("1")
     try {
-        JWT.verify(req.headers.authorization, JWT_SECRET, async (err, decoded) => {
+        JWT.verify(req.headers.authorization, JWT_SECRET, async ( decoded, err) => {
             if (err) {
+                // console.log("2")
                 return res.status(401).json({ message: err });
             } else {
+                // console.log("3")
                 let { lhId } = decoded;
                 let loginHistory = await LoginHistory.findById(lhId);
+                // console.log(loginHistory)
                 if (!loginHistory) {
                     return res.status(401).json({ message: 'Unauthorized: Invalid token2' });
                 };
