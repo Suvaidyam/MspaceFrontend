0d56ca866fe89577b5495d73618504c1fdd116b4
abhirock74 <abhishek.suvaidyam.com>
2022-08-19 13:57:35 +0530

    add register user and company logic

--- Backend/Api/Auth/Register.js
+++ Backend/Api/Auth/Register.js
@@ -5,26 +5,32 @@ const Register = async (req, res) => {
     try {
         let { name, password, email, company, companyName, companyCode } = req.body;
         let userType = "EMPLOYEE";
-        if (!name || !password || !email || !company) {
-            return res.status(400).json({ message: "name company,password,email, companyName,companyCode  is required" })
+        if (!name || !password || !email) {
+            return res.status(400).json({ message: "name company_refId,password,email  is required" })
         };
+        if (company || (companyName && companyCode)) {
         let user = await User.findOne({ email, password });
-        if (user) {
-            return res.status(400).json({ message: "Dublicate User" });
-        } else {
-            if (!company) {
-                let com = await Company.findOne({ code: companyCode });
-                if (com) {
-                    return res.status(400).json({ message: "Company Code is already exists" });
-                } else {
-                    let newCompany = await Company.create({ name: companyName, code: companyCode });
-                    userType = "COMPANY_ADMIN";
-                    company = newCompany._id;
+            if (user) {
+                return res.status(400).json({ message: "Dublicate User" });
+            } else {
+
+                if (!company) {
+                    let com = await Company.findOne({ code: companyCode });
+                    if (com) {
+                        return res.status(400).json({ message: "Company Code is already exists" });
+                    } else {
+                        let newCompany = await Company.create({ name: companyName, code: companyCode });
+                        userType = "COMPANY_ADMIN";
+                        company = newCompany._id;
+                    }
                 }
+                user = await User.create({ name, password, email, company, userType });
+                return res.status(200).json({ message: "User Successfully Created", user: user });
             }
-            user = await User.create({ name, password, email, company, userType });
-            return res.status(200).json({ message: "User Successfully Created", user: user });
+        } else {
+            return res.status(400).json({ message: "campany (companyName,companyCode ) detail reqiured" })
         }
+
     } catch (error) {
         return res.status(500).json({ message: error.message });
     }
