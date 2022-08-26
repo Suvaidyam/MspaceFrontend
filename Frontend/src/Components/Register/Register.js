273d551f8ed20c3700bfef8acb33f02ee022c00c
abhirock74 <abhishek.suvaidyam.com>
2022-08-26 18:33:15 +0530

    add logo and register some code

--- Frontend/src/Components/Register/Register.js
+++ Frontend/src/Components/Register/Register.js
@@ -1,36 +1,38 @@
 import React from 'react'
-
+import Logo from '../../Assets/logo.png'
 const Register = () => {
   return (
     <>
-    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    {/* <div className="grid grid-cols-1 grid-rows-2 lg:grid-rows-1 lg:grid-cols-12 md:grid-cols-12 ">
+  <div className='col-span-5 bg-[#5800FF]'>01</div>
+  <div className='col-span-7'>09</div>
+</div> */}
+    <div className=" w-full flex">
+    <div className="col-3  min-h-full w-[45%] items-center justify-center bg-indigo-600">
+           <img className='w-[20%] mt-9 ml-24' src={Logo} alt="Logo" />
+        </div>
+        <div className="col-1 min-h-screen w-[55%] px-40">
+        <div className="min-h-full flex  py-12 px-4 sm:px-6 lg:px-8">
   <div className="max-w-md w-full space-y-8">
     <div>
-      <img
-        className="mx-auto h-12 w-auto"
+      <imgjustify-center
+        className="my-6 h-12 w-auto"
         src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
         alt="Workflow"
       />
-      <h2 className="mt-6 text-center text-3xl tracking-tight font-bold text-gray-900">
-        Sign in to your account
-      </h2>
-      <p className="mt-2 text-center text-sm text-gray-600">
-        Or
-        <a
-          href="#"
-          className="font-medium text-indigo-600 hover:text-indigo-500"
-        >
-          {" "}
-          start your 14-day free trial{" "}
-        </a>
-      </p>
+      <h1 className="mt-6  text-3xl tracking-tight font-bold text-gray-900">
+      Sign up for an Account
+      </h1>
+      <h4 className="mt-2  text-md text-gray-600">
+      Please provide following details to continue
+      </h4>
     </div>
     <form className="mt-8 space-y-6" action="#" method="POST">
       <input type="hidden" name="remember" defaultValue="true" />
       <div className="rounded-md shadow-sm -space-y-px">
         <div>
-          <label htmlFor="email-address" className="sr-only">
-            Email address
+          <label htmlFor="email-address" className="text-lg ">
+            Email 
           </label>
           <input
             id="email-address"
@@ -38,12 +40,12 @@ const Register = () => {
             type="email"
             autoComplete="email"
             required=""
-            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            className="appearance-none rounded-sm relative block w-full px-3 mt-2 mb-6 py-3 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
             placeholder="Email address"
           />
         </div>
         <div>
-          <label htmlFor="password" className="sr-only">
+          <label htmlFor="password" className="text-lg">
             Password
           </label>
           <input
@@ -52,7 +54,7 @@ const Register = () => {
             type="password"
             autoComplete="current-password"
             required=""
-            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            className="appearance-none mt-2 mb-6 rounded-sm relative block w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
             placeholder="Password"
           />
         </div>
@@ -86,7 +88,7 @@ const Register = () => {
       <div>
         <button
           type="submit"
-          className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className="group relative w-full flex justify-center py-3 px-4 rounded-sm border border-transparent text-md font-medium  text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           <span className="absolute left-0 inset-y-0 flex items-center pl-3">
             {/* Heroicon name: solid/lock-closed */}
@@ -104,12 +106,23 @@ const Register = () => {
               />
             </svg>
           </span>
-          Sign in
+          LOG IN
         </button>
+      </div>
+      <div>
+        <p className='text-center text-xs font-medium'>Or Login Using</p>
+      </div>
+      <div className="flex justify-center">
+        <div className='border-2 rounded-sm text-xs font-medium px-4 py-3 mx-2'>Contnue with Google</div>
+        <div className='border-2 rounded-sm text-xs font-medium px-4 py-3 mx-2'>Contnue with Microsoft</div>
       </div>
     </form>
   </div>
 </div>
+        </div>
+        
+     </div>
+
 
     </>
   )
