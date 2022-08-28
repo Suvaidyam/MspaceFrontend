77e84c6d700d09def0fae5f787e871fca2edede3
ani07ket <aniket.suvaidyam@gmail.com>
2022-08-28 17:21:36 +0530

    removed extra

--- Frontend/src/Components/HomeUI/HomePage.js
+++ Frontend/src/Components/HomeUI/HomePage.js
@@ -1,7 +1,7 @@
 import { Fragment, useState } from 'react'
-import { Dialog, Disclosure, Menu, Transition } from '@headlessui/react'
+import { Dialog, Menu, Transition } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
-import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from '@heroicons/react/20/solid'
+import { ChevronDownIcon, FunnelIcon, Squares2X2Icon } from '@heroicons/react/20/solid'
 
 import Navbar from '../Navbar/Navbar'
 import Overview from './Overview/Overview'
