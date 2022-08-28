33b35b1cd9f0524720fbde4064689f5251cdd063
abhirock74 <abhishek.suvaidyam.com>
2022-08-28 13:50:55 +0530

    add main page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,11 @@
 import Router from './Components/Router/Router';
 import Footer from './Components/Footer/Footer';
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (
     <>
+    <Navbar/>
      <Router/>
      <Footer/>
     </>
