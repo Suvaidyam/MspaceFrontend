dd10183d85f66abb0e30bf38975c33bbd82c80ed
abhirock74 <abhishek.suvaidyam.com>
2022-08-28 13:59:26 +0530

    add structure

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,9 @@
 import Router from './Components/Router/Router';
 import Footer from './Components/Footer/Footer';
-import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (
     <>
-    <Navbar/>
      <Router/>
      <Footer/>
     </>
--- Frontend/src/Components/HomeUI/HomePage.js
+++ Frontend/src/Components/HomeUI/HomePage.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import SideBar from './LeftSideBar/SideBar'
+import Overview from './Overview/Overview'
 
 
 const HomePage = () => {
   return (
    <>
+   <Navbar/>
+   <div className='flex'>
+    <div className='w-[20%]'>
+    <SideBar/>
+    </div>
+    <div className='w-[80%] bg-slate-50'>
+    <Overview/>
+    </div>
+   </div>
   
 
    </>
