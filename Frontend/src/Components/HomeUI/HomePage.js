54daf16b3445023eafc5e94b942aac10ec1567f3
ani07ket <aniket.suvaidyam@gmail.com>
2022-08-28 14:10:06 +0530

    overview page card is complete

--- Frontend/src/Components/HomeUI/HomePage.js
+++ Frontend/src/Components/HomeUI/HomePage.js
@@ -6,19 +6,19 @@ import Overview from './Overview/Overview'
 
 const HomePage = () => {
   return (
-   <>
-   <Navbar/>
-   <div className='flex'>
-    <div className='w-[20%]'>
-    <SideBar/>
-    </div>
-    <div className='w-[80%] bg-slate-50'>
-    <Overview/>
-    </div>
-   </div>
-  
+    <>
+      <Navbar />
+      <div className='flex'>
+        <div className='w-[20%]'>
+          <SideBar />
+        </div>
+        <div className='w-[80%] bg-slate-50'>
+          <Overview />
+        </div>
+      </div>
 
-   </>
+
+    </>
   )
 }
 
--- Frontend/src/Components/HomeUI/Overview/Overview.js
+++ Frontend/src/Components/HomeUI/Overview/Overview.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Img from '../../../Assets/room.png'
 
 const Overview = () => {
   return (
-    <div>Overview</div>
+    <div className="flex  ">
+      <div className="w-[284px] h-[280px]  bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img className="  p-1 items-center rounded-lg  w-[284px] h-[152px]" src={Img} alt />
+        </a>
+        <div>
+          <h1 className="p-2 ml-1">Meeting Area <span className='ml-20'>Capacity:4</span></h1>
+        </div>
+      </div>
+
+
+
+
+    </div>
   )
 }
 
