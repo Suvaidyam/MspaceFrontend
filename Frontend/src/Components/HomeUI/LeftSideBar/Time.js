d1ae45e2a844673988f6213e87a0e81a196144e9
ani07ket <aniket.suvaidyam@gmail.com>
2022-08-29 12:00:16 +0530

    internal error fixed

--- Frontend/src/Components/HomeUI/LeftSideBar/Time.js
+++ Frontend/src/Components/HomeUI/LeftSideBar/Time.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Time = () => {
-    return (
+    return (<>
         <div className='mt-3 items-center justify-center flex flex-wrap w-[100%] px-0 lg:px-8'>
 
             <button type="button" class="py-3 px-6 ml-4 mb-2 text-sm font-medium text-[#3CCF4E] focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">6pm-8pm</button>
@@ -10,7 +10,17 @@ const Time = () => {
             <button type="button" class="py-3 px-6 ml-4 mb-2 text-sm font-medium text-[#EF444A] focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">6pm-8pm</button>
 
             <button type="button" class="py-3 px-6 ml-4  mb-2 text-sm font-medium text-[#3CCF4E] focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">8pm-10pm</button>
+
+
+
+
+
         </div>
+        <div className='mt-3 items-center justify-center flex flex-wrap w-[100%] px-0 lg:px-8'>
+            <button type="button" class=" px-16 py-3    flex mb-2 text-sm font-medium text-[#3CCF4E] focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"> <svg className=" w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg> <span className='text-lg'>Find Space</span> </button>
+
+        </div>
+    </>
     )
 }
 
