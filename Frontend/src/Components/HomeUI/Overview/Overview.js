85af4d0c7c6ed8997a117fb3ab876848446b9bd6
ani07ket <aniket.suvaidyam@gmail.com>
2022-08-29 17:15:02 +0530

    add hover effect on card

--- Frontend/src/Components/HomeUI/Overview/Overview.js
+++ Frontend/src/Components/HomeUI/Overview/Overview.js
@@ -6,7 +6,7 @@ const Overview = () => {
     <>
       <div className="flex justify-center items-center flex-wrap   pb-8">
 
-        <div className="w-[270px] h-[250px]  m-3 ml-8   bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className=" transform transition duration-1000 hover:scale-105    w-[270px] h-[250px]  m-3 ml-8   bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
             <img className="  p-1 items-center rounded-lg  w-[284px] h-[152px]" src={Img} alt />
           </a>
@@ -18,7 +18,7 @@ const Overview = () => {
           </div>
         </div>
 
-        <div className="w-[270px] h-[250px]  m-3 ml-8   bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="  transform transition duration-1000 hover:scale-105  w-[270px] h-[250px]  m-3 ml-8   bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
             <img className="  p-1 items-center rounded-lg  w-[284px] h-[152px]" src={Img} alt />
           </a>
@@ -29,7 +29,7 @@ const Overview = () => {
             <button type="button" class="text-white  flex bg-gray-400  focus:outline-none font-medium  text-sm px-14 ml-2 py-2 text-center mr-2 mb-2"><svg className=" w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z"></path></svg> <span className='text-lg'>Room is Full</span></button>
           </div>
         </div>
-        <div className="w-[270px] h-[250px]  m-3 ml-8   bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="   transform transition duration-1000 hover:scale-105  w-[270px] h-[250px]  m-3 ml-8   bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
             <img className="  p-1 items-center rounded-lg  w-[284px] h-[152px]" src={Img} alt />
           </a>
@@ -41,7 +41,7 @@ const Overview = () => {
           </div>
         </div>
 
-        <div className="w-[270px] h-[250px]  m-3 ml-8   bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="  transform transition duration-1000 hover:scale-105   w-[270px] h-[250px]  m-3 ml-8   bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
             <img className="  p-1 items-center rounded-lg  w-[284px] h-[152px]" src={Img} alt />
           </a>
@@ -53,7 +53,7 @@ const Overview = () => {
           </div>
         </div>
 
-        <div className="w-[270px] h-[250px]  m-3 ml-8   bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="transform transition duration-1000 hover:scale-105    w-[270px] h-[250px]  m-3 ml-8   bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
             <img className="  p-1 items-center rounded-lg  w-[284px] h-[152px]" src={Img} alt />
           </a>
@@ -64,7 +64,7 @@ const Overview = () => {
             <button type="button" class="text-white  flex bg-gray-400  focus:outline-none font-medium  text-sm px-14 ml-2 py-2 text-center mr-2 mb-2"><svg className=" w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z"></path></svg> <span className='text-lg'>Room is Full</span></button>
           </div>
         </div>
-        <div className="w-[270px] h-[250px]  m-3 ml-8   bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="   transform transition duration-1000 hover:scale-105  w-[270px] h-[250px]  m-3 ml-8   bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
             <img className="  p-1 items-center rounded-lg  w-[284px] h-[152px]" src={Img} alt />
           </a>
