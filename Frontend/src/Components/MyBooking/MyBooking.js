80b5913c8c3e07c4d97103ac28fde872b40e3380
rahul.suvaidyam <rahul.suvaidyam@gmail.com>
2022-09-01 12:57:26 +0530

    change home overview mybooking page btn color

--- Frontend/src/Components/MyBooking/MyBooking.js
+++ Frontend/src/Components/MyBooking/MyBooking.js
@@ -6,7 +6,7 @@ const MyBooking = () => {
   return (
     <>
       <Navbar/>
-      <Overview book={'Booked'}/>
+      <Overview book={'Booked'} bgcolor={'text-white  flex bg-[#979797]  focus:outline-none font-medium justify-center  text-sm w-[240px] ml-2 py-2 text-center mr-2 mb-2'}/>
     </>
   )
 }
