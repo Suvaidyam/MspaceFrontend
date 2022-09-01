7abaa49ccf2b319b74e42241028502e035a1c0c0
abhirock74 <abhishek.suvaidyam.com>
2022-09-01 18:31:40 +0530

    register page login button added

--- Frontend/src/Components/Register/Other.js
+++ Frontend/src/Components/Register/Other.js
@@ -38,11 +38,11 @@ const Other = (props) => {
                     />
                     <ErrorMessage className='text-red-600 mb-4' name='companyCode' component='p'/>
                   </div>
-                  <div className="mt-6">
+                  {/* <div className="mt-6">
                     <label htmlFor="companyCode" className="block">Company Logo</label>
-                    {/* <FileUploder setcompanyLogo={props.setcompanyLogo}/> */}
+                    <FileUploder setcompanyLogo={props.setcompanyLogo}/>
                     <ErrorMessage className='text-red-600 mb-4' name='companyCode' component='p'/>
-                  </div>
+                  </div> */}
                   
                   </div>
     </>
