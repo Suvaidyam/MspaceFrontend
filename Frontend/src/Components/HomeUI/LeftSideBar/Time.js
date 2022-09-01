0bed96bb2c0e7e8a7d729d4af9e164708c5241d5
abhirock74 <abhishek.suvaidyam.com>
2022-09-01 15:15:52 +0530

    add time slider for booking meeting

--- Frontend/src/Components/HomeUI/LeftSideBar/Time.js
+++ Frontend/src/Components/HomeUI/LeftSideBar/Time.js
@@ -5,13 +5,93 @@ import Slider from '@mui/material/Slider';
 function valuetext(value) {
     return `${value}°C`;
 }
+const marks = [
+    {
+      value: 10,
+      label: '10',
+    },
+    {
+      value: 11,
+      label: '11',
+    },
+    {
+      value: 12,
+      label: '12',
+    },
+    {
+      value: 13,
+      label: '1',
+    },
+    {
+      value: 14,
+      label: '2',
+    },
+    {
+      value: 15,
+      label: '3',
+    },
+    {
+      value: 16,
+      label: '4',
+    },
+    {
+      value: 17,
+      label: '5',
+    },
+    {
+      value: 18,
+      label: '6',
+    },
+    {
+      value: 19,
+      label: '7',
+    },
+    {
+      value: 20,
+      label: '8',
+    },
+    {
+      value: 21,
+      label: '9',
+    },
+    {
+      value: 22,
+      label: '10',
+    },
+  ];
 
 export default function Time() {
-    const [value, setValue] = React.useState([20, 37]);
+    const [value, setValue] = React.useState([12, 18]);
+    const [fromTime, setfromTime] = React.useState(12);
+    const [toTime, settoTime] = React.useState(18);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
+        // setfromTime(value[0])
+        settoTime(value[1])
+        getTimeValue()  
     };
+    const getTimeValue = ()=>{
+        if(value[0] === 10){
+            setfromTime("10:00 Am")
+        }else if(value[0] === 10.5){
+            setfromTime("10:30 Am")
+        }else if(value[0] === 11){
+            setfromTime("11:00 Am")
+        }else if(value[0] === 11.5){
+            setfromTime("11:30 Am")
+        }else if(value[0] === 12){
+            setfromTime("12:00 Pm")
+        }
+        else if(value[0] === 12){
+            setfromTime("12:00 Pm")
+        }else if(value[0] === 13){
+            setfromTime("01:00 Pm")
+        }else if(value[0] === 14){
+            setfromTime("02:30 Am")
+        }
+    }
+    
 
     return (
         <>
@@ -20,23 +100,28 @@ export default function Time() {
                 <div className="flex gap-2  justify-center items-center px-3 ">
 
                     <div>
-                        <input type="text" id="first_name" className="bg-white border shadow border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="From" required />
+                        <input type="text" value={fromTime} id="first_name" className="bg-white border shadow border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="From" required />
                     </div>
                     <div>
 
-                        <input type="text" id="last_name" className="bg-White border shadow border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="To" required />
+                        <input type="text" value={toTime} id="last_name" className="bg-White border shadow border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="To" required />
                     </div>
                 </div>
             </div>
 
 
-            <Box className='flex justify-center items-center  px-3'>
+            <Box className='flex justify-center items-center mx-5'>
                 <Slider
                     getAriaLabel={() => 'Temperature range'}
                     value={value}
                     onChange={handleChange}
-                    valueLabelDisplay="auto"
+                    step={0.5}
                     getAriaValueText={valuetext}
+                    max={22}
+                    min={10}
+                    marks={marks}
+                    // valueLabelDisplay="auto"
+                    // scale={Time}
                 />
             </Box>
         </>
