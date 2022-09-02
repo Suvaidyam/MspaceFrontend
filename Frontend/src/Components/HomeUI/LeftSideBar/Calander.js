8813662e5672b62e681d5c109b1d67e98be354a1
ani07ket <aniket.suvaidyam@gmail.com>
2022-09-02 12:33:45 +0530

    some change

--- Frontend/src/Components/HomeUI/LeftSideBar/Calander.js
+++ Frontend/src/Components/HomeUI/LeftSideBar/Calander.js
@@ -17,7 +17,7 @@ export default function Calander() {
         <div className="flex justify-center items-center px-3 pt-5  shadow-transparent border-none">
 
           <Calendar
-            className="px-3   py-3 border-none shadow-transparent rounded "
+            className="px-3    border-none shadow-transparent rounded "
             value={dateState}
             onChange={changeDate}
           />
--- Frontend/src/Components/HomeUI/LeftSideBar/Time.js
+++ Frontend/src/Components/HomeUI/LeftSideBar/Time.js
@@ -7,58 +7,58 @@ function valuetext(value) {
 }
 const marks = [
     {
-      value: 10,
-      label: '10',
+        value: 10,
+        label: '10',
     },
     {
-      value: 11,
-      label: '11',
+        value: 11,
+        label: '11',
     },
     {
-      value: 12,
-      label: '12',
+        value: 12,
+        label: '12',
     },
     {
-      value: 13,
-      label: '1',
+        value: 13,
+        label: '1',
     },
     {
-      value: 14,
-      label: '2',
+        value: 14,
+        label: '2',
     },
     {
-      value: 15,
-      label: '3',
+        value: 15,
+        label: '3',
     },
     {
-      value: 16,
-      label: '4',
+        value: 16,
+        label: '4',
     },
     {
-      value: 17,
-      label: '5',
+        value: 17,
+        label: '5',
     },
     {
-      value: 18,
-      label: '6',
+        value: 18,
+        label: '6',
     },
     {
-      value: 19,
-      label: '7',
+        value: 19,
+        label: '7',
     },
     {
-      value: 20,
-      label: '8',
+        value: 20,
+        label: '8',
     },
     {
-      value: 21,
-      label: '9',
+        value: 21,
+        label: '9',
     },
     {
-      value: 22,
-      label: '10',
+        value: 22,
+        label: '10',
     },
-  ];
+];
 
 export default function Time() {
     const [value, setValue] = React.useState([10, 18]);
@@ -71,124 +71,124 @@ export default function Time() {
         // console.log(newValue)
         // setfromTime(value[0])
         // settoTime(value[1])
-        getFromTimeValue()  
+        getFromTimeValue()
         getToTimeValue()
     };
-    const getFromTimeValue = ()=>{
-        if(value[0] === 10){
+    const getFromTimeValue = () => {
+        if (value[0] === 10) {
             setfromTime("10:00 Am")
-        }else if(value[0] === 10.5){
+        } else if (value[0] === 10.5) {
             setfromTime("10:30 Am")
-        }else if(value[0] === 11){
+        } else if (value[0] === 11) {
             setfromTime("11:00 Am")
-        }else if(value[0] === 11.5){
+        } else if (value[0] === 11.5) {
             setfromTime("11:30 Am")
-        }else if(value[0] === 12){
+        } else if (value[0] === 12) {
             setfromTime("12:00 Pm")
-        }else if(value[0] === 12.5){
+        } else if (value[0] === 12.5) {
             setfromTime("12:30 Pm")
-        }else if(value[0] === 13){
+        } else if (value[0] === 13) {
             setfromTime("01:00 Pm")
-        }else if(value[0] === 13.5){
+        } else if (value[0] === 13.5) {
             setfromTime("01:30 Pm")
-        }else if(value[0] === 14){
+        } else if (value[0] === 14) {
             setfromTime("02:00 Pm")
-        }else if(value[0] === 14.5){
+        } else if (value[0] === 14.5) {
             setfromTime("02:30 Pm")
-        }else if(value[0] === 15){
+        } else if (value[0] === 15) {
             setfromTime("03:00 Pm")
-        }else if(value[0] === 15.5){
+        } else if (value[0] === 15.5) {
             setfromTime("03:30 Pm")
-        }else if(value[0] === 16){
+        } else if (value[0] === 16) {
             setfromTime("04:00 Pm")
-        }else if(value[0] === 16.5){
+        } else if (value[0] === 16.5) {
             setfromTime("04:30 Pm")
-        }else if(value[0] === 17){
+        } else if (value[0] === 17) {
             setfromTime("05:00 Pm")
-        }else if(value[0] === 17.5){
+        } else if (value[0] === 17.5) {
             setfromTime("05:30 Pm")
-        }else if(value[0] === 18){
+        } else if (value[0] === 18) {
             setfromTime("06:00 Pm")
-        }else if(value[0] === 18.5){
+        } else if (value[0] === 18.5) {
             setfromTime("06:30 Pm")
-        }else if(value[0] === 19){
+        } else if (value[0] === 19) {
             setfromTime("07:00 Pm")
-        }else if(value[0] === 19.5){
+        } else if (value[0] === 19.5) {
             setfromTime("07:30 Pm")
-        }else if(value[0] === 20){
+        } else if (value[0] === 20) {
             setfromTime("08:00 Pm")
-        }else if(value[0] === 20.5){
+        } else if (value[0] === 20.5) {
             setfromTime("08:30 Pm")
-        }else if(value[0] === 21){
+        } else if (value[0] === 21) {
             setfromTime("09:00 Pm")
-        }else if(value[0] === 21.5){
+        } else if (value[0] === 21.5) {
             setfromTime("09:30 Pm")
-        }else if(value[0] === 22){
+        } else if (value[0] === 22) {
             setfromTime("10:00 Pm")
-        }else{
+        } else {
             setfromTime(null)
         }
     }
-    const getToTimeValue = ()=>{
-        if(value[1] === 10){
+    const getToTimeValue = () => {
+        if (value[1] === 10) {
             settoTime("10:00 Am")
-        }else if(value[1] === 10.5){
+        } else if (value[1] === 10.5) {
             settoTime("10:30 Am")
-        }else if(value[1] === 11){
+        } else if (value[1] === 11) {
             settoTime("11:00 Am")
-        }else if(value[1] === 11.5){
+        } else if (value[1] === 11.5) {
             settoTime("11:30 Am")
-        }else if(value[1] === 12){
+        } else if (value[1] === 12) {
             settoTime("12:00 Pm")
-        }else if(value[1] === 12.5){
+        } else if (value[1] === 12.5) {
             settoTime("12:30 Pm")
-        }else if(value[1] === 13){
+        } else if (value[1] === 13) {
             settoTime("01:00 Pm")
-        }else if(value[1] === 13.5){
+        } else if (value[1] === 13.5) {
             settoTime("01:30 Pm")
-        }else if(value[1] === 14){
+        } else if (value[1] === 14) {
             settoTime("02:00 Pm")
-        }else if(value[1] === 14.5){
+        } else if (value[1] === 14.5) {
             settoTime("02:30 Pm")
-        }else if(value[1] === 15){
+        } else if (value[1] === 15) {
             settoTime("03:00 Pm")
-        }else if(value[1] === 15.5){
+        } else if (value[1] === 15.5) {
             settoTime("03:30 Pm")
-        }else if(value[1] === 16){
+        } else if (value[1] === 16) {
             settoTime("04:00 Pm")
-        }else if(value[1] === 16.5){
+        } else if (value[1] === 16.5) {
             settoTime("04:30 Pm")
-        }else if(value[1] === 17){
+        } else if (value[1] === 17) {
             settoTime("05:00 Pm")
-        }else if(value[1] === 17.5){
+        } else if (value[1] === 17.5) {
             settoTime("05:30 Pm")
-        }else if(value[1] === 18){
+        } else if (value[1] === 18) {
             settoTime("06:00 Pm")
-        }else if(value[1] === 18.5){
+        } else if (value[1] === 18.5) {
             settoTime("06:30 Pm")
-        }else if(value[1] === 19){
+        } else if (value[1] === 19) {
             settoTime("07:00 Pm")
-        }else if(value[1] === 19.5){
+        } else if (value[1] === 19.5) {
             settoTime("07:30 Pm")
-        }else if(value[1] === 20){
+        } else if (value[1] === 20) {
             settoTime("08:00 Pm")
-        }else if(value[1] === 20.5){
+        } else if (value[1] === 20.5) {
             settoTime("08:30 Pm")
-        }else if(value[1] === 21){
+        } else if (value[1] === 21) {
             settoTime("09:00 Pm")
-        }else if(value[1] === 21.5){
+        } else if (value[1] === 21.5) {
             settoTime("09:30 Pm")
-        }else if(value[1] === 22){
+        } else if (value[1] === 22) {
             settoTime("10:00 Pm")
-        }else{
+        } else {
             settoTime(null)
         }
     }
-    
+
 
     return (
         <>
-            <div className='justify-center items-center pb-1'>
+            <div className='justify-center items-center '>
                 <label className="block px-3  py-3  text-sm font-bold text-gray-700 dark:text-gray-300">Select Time</label>
                 <div className="flex gap-2  justify-center items-center px-3 ">
 
@@ -213,8 +213,8 @@ export default function Time() {
                     max={22}
                     min={10}
                     marks={marks}
-                    // valueLabelDisplay="auto"
-                    // scale={Time}
+                // valueLabelDisplay="auto"
+                // scale={Time}
                 />
             </Box>
         </>
