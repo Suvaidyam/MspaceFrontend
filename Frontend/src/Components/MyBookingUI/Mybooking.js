02887cd7a4f266d9c3011ece72a610f7ad124cf8
rahul.suvaidyam <rahul.suvaidyam@gmail.com>
2022-09-08 15:24:39 +0530

    add right contents

--- Frontend/src/Components/MyBookingUI/Mybooking.js
+++ Frontend/src/Components/MyBookingUI/Mybooking.js
@@ -9,9 +9,9 @@ function MyBooking() {
   return (
     <>
       <Navbar />
-      <div className='flex h-screen'>
+      <div className='flex'>
 
-        <div className='w-[22%] bg-slate-100 shadow-inner  '>
+        <div className='w-[22%]  shadow-inner  '>
 
           <h1 className='px-3 pt-4 text-xl font-bold font-sans' >Book Spaces</h1>
           <h1 className='px-3 pt-4 text-sm font-bold font-sans' >Your Booking</h1>
@@ -19,11 +19,11 @@ function MyBooking() {
 
         </div>
 
-        <div className='w-[78%] flex'>
-          <div className='w-[80%]'>
+        <div className='w-[78%] flex bg-gray-50'>
+          <div className='w-[75%]'>
             <CenterBox />
           </div>
-          <div className='w-[20%] border'>
+          <div className='w-[25%]'>
             <RightSideBar />
           </div>
         </div>
