7ec0c4fef565aafdf388d0d25804447961441d6b
ani07ket <aniket.suvaidyam@gmail.com>
2022-09-08 11:40:47 +0530

    nav skeltion ready

---
+++ Frontend/src/Components/Skeleton/nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div>
+            <div className='flex h-[75px]  '>
+
+                <div className='h-12  animate-pulse leading-relaxed  bg-gray-200 mt-3.5 ml-14 w-28 py-3'></div>
+                <div className='h-4  animate-pulse leading-relaxed  bg-gray-200 mt-6 ml-10 w-20 py-3'></div>
+                <div className='h-4  animate-pulse leading-relaxed  bg-gray-200 mt-6 ml-6 w-20 py-3'></div>
+                <div className='h-10  rounded-lg animate-pulse leading-relaxed  bg-gray-200 mt-4 ml-[495px] w-48 py-3'></div>
+                <div className='h-8  rounded-full animate-pulse leading-relaxed  bg-gray-200 mt-5 ml-4 w-8 py-3'></div>
+                <div className='h-10  animate-pulse leading-relaxed  bg-gray-200 mt-4 ml-4 w-10 rounded-full py-3'></div>
+                <div className='h-4  animate-pulse leading-relaxed  bg-gray-200 mt-6 ml-4 w-24  py-3'></div>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Nav
