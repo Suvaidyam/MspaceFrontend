cf36b36160d2b525d565bb940a9461aab2b17a5e
abhirock74 <abhishek.suvaidyam.com>
2022-09-08 07:30:17 +0530

    add

--- Frontend/src/Components/HomeUI/LeftSideBar/SideBar.js
+++ Frontend/src/Components/HomeUI/LeftSideBar/SideBar.js
@@ -5,10 +5,14 @@ import Findbtn from './Findbtn'
 
 const SideBar = () => {
   const [bookingTime , setbookingTime] =useState(null)
-  const [bookingDate , setbookingDate] =useState(null)
+  const [bookingDate , setbookingDate] =useState()
   const [capacity , setcapacity] =useState(null)
   
   const getBookingData=()=>{
+    // if(bookingDate){
+    // const firstDate=  bookingDate.slice(0, 15)
+    // console.log(firstDate)
+    // }
     console.log("booking time", bookingTime)
   console.log("bookingDate", bookingDate)
   console.log("capacity",capacity)
