3ef2f8196a172e08430596aae5bf7d195ffafc55
rahul.suvaidyam <rahul.suvaidyam@gmail.com>
2022-09-09 12:54:27 +0530

    add all today previous upcoming cancelled componet is done

--- Frontend/src/Components/MyBookingUI/Today/Today.js
+++ Frontend/src/Components/MyBookingUI/Today/Today.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Cards from '../AllCards/Cards'
 
 const Today = () => {
   return (
-    <div>Today</div>
+    <>
+    <div className="mt-12" >
+    <Cards btn={'Today'} btncolor={'mx-2 bg-green-200 px-4 rounded-md border-2 text-green-500 border-green-500'}/>
+    </div>
+    <div className="my-6" >
+    <Cards btn={'Today'} btncolor={'mx-2 bg-green-200 px-4 rounded-md border-2 text-green-500 border-green-500'}/>
+    </div>
+    <div className="mb-4">
+    <Cards btn={'Today'} btncolor={'mx-2 bg-green-200 px-4 rounded-md border-2 text-green-500 border-green-500'}/>
+    </div>
+      </>
   )
 }
 
