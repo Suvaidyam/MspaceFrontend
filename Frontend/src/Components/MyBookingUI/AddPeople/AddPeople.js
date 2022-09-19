1c9c6d5ccef9142cae61c8183cc32bba75f56096
rahul.suvaidyam <rahul.suvaidyam@gmail.com>
2022-09-19 13:13:21 +0530

    icon  add

--- Frontend/src/Components/MyBookingUI/AddPeople/AddPeople.js
+++ Frontend/src/Components/MyBookingUI/AddPeople/AddPeople.js
@@ -1,18 +1,15 @@
 import React from 'react'
+import { AiOutlineClose } from 'react-icons/ai'
 
 const AddPeople = () => {
     return (
         <>
             <div className="w-full">
-                <div className="w-[500px] p-3 rounded-md bg-gray-300 flex-col flex gap-2">
+                <div className="w-[500px] px-3 py-2 rounded-md bg-gray-300 flex-col flex gap-2">
                     <div className="flex items-center justify-between">
                         <p className='font-semibold text-gray-700'>Add People</p>
                         <p className='font-semibold cursor-pointer'>
-                            <svg id="clear_black_24dp" xmlns="http://www.w3.org/2000/svg" width="18.046" height="18.046" viewBox="0 0 13.046 13.046" >
-                                <path id="Path_887" data-name="Path 887" d="M0,0H13.046V13.046H0Z" fill="none" />
-                                <path id="Path_888" data-name="Path 888" d="M12.61,5.766,11.844,5,8.805,8.039,5.766,5,5,5.766,8.039
-                                ,8.805,5,11.844l.766.766L8.805,9.572l3.039,3.039.766-.766L9.572,8.805Z" transform="translate(-2.282 -2.282)" fill="#3330e4" />
-                            </svg>
+                            <AiOutlineClose />
                         </p>
                     </div>
                     <div className='flex flex-col gap-3'>
--- Frontend/src/Components/MyBookingUI/Data.js
+++ Frontend/src/Components/MyBookingUI/Data.js
@@ -3,18 +3,30 @@ export const meeting = [
         id: 1,
         time: 9,
         date: 'Setptember 5, 2022',
-        meetingArea: 'Meeting Area 1'
+        meetingArea: 'Meeting Area 1',
+        peopleimg1: 'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        peopleimg2: 'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        peopleimg3: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
+        peopleimg4: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     },
     {
         id: 2,
         time: 10,
         date: 'Setptember 5, 2022',
-        meetingArea: 'Meeting Area 2'
+        meetingArea: 'Meeting Area 2',
+        peopleimg1: 'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        peopleimg2: 'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        peopleimg3: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
+        peopleimg4: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     },
     {
         id: 3,
         time: 11,
         date: 'Setptember 8, 2022',
-        meetingArea: 'Meeting Area 3'
+        meetingArea: 'Meeting Area 3',
+        peopleimg1: 'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        peopleimg2: 'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        peopleimg3: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
+        peopleimg4: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     },
 ]
