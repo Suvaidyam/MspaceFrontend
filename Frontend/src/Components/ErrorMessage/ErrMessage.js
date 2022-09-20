ab94ff36e103053bd34853707728e439f2585b70
abhirock74 <abhishek.suvaidyam.com>
2022-09-20 07:49:49 +0530

    remove error message component

--- Frontend/src/Components/ErrorMessage/ErrMessage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ErrMessage = (props) => {
-  return (
-    <div className='absolute bg-red-600 text-white bottom-[-35%] rounded-none'>{props.error}</div>
-  )
-}
-
-export default ErrMessage
